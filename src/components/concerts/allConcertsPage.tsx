13b95893c16256b152311c3de124889f71e6079d
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2019-07-09 13:06:26 -0400

    Replace main navigation with React Router routes

---
+++ src/components/concerts/allConcertsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// TODO: Build out a dashboard landing page with current user's info
+// https://trello.com/c/SlbK5LEA/17-pages-all-concerts
+export function AllConcertsPage(): JSX.Element {
+    return <div>All Concerts</div>
+}
---
+++ src/components/dashboardPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// TODO: Build out a dashboard landing page with current user's info
+// https://trello.com/c/O3IV0Oib/16-pages-dashboard
+export function DashboardPage(): JSX.Element {
+    return <div>Dashboard</div>
+}
