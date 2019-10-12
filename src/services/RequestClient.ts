7761b0f24d2ec385091ea2c6f7df9eb84ff71753
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2019-10-12 10:21:19 -0400

    Added basic API client wrapper for get, post and put methods

---
+++ src/services/RequestClient.ts
@@ -0,0 +1,32 @@
+const JSONHeaders = {"Content-Type": "application/json"}
+
+export const APIGet = async<T>(url: string): Promise<T> => {
+    const args = {method: "GET", headers: JSONHeaders}
+    return await APIRequest<T>(new Request(url, args))
+}
+
+export const APIPost = async<T, U>(url: string, body: U): Promise<T> => {
+    const args = {method: "POST", headers: JSONHeaders, body: JSON.stringify(body)}
+    return await APIRequest<T>(new Request(url, args))
+}
+
+export const APIPut = async<T, U>(url: string, body: U): Promise<T> => {
+    const args = {method: "PUT", headers: JSONHeaders, body: JSON.stringify(body)}
+    return await APIRequest<T>(new Request(url, args))
+}
+
+// Write test with JEST to handle bad response
+const APIRequest = <T>(request: RequestInfo): Promise<T> => {
+    return new Promise((resolve, reject) => {
+        let resp: Response
+        fetch(request)
+            .then(response => {
+                resp = response
+                return response.json()
+            })
+            .then(body => {
+                return resp.ok ? resolve(body) : reject(resp)
+            }
+        ).catch(error => reject(error))
+    })
+}
