38a7ac38c9d83626bd5538fe2aa21da3179bec04
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2019-12-30 11:13:14 -0500

    Get leaderboard endpoint added

---
+++ src/services/APIUserFetcher.ts
@@ -0,0 +1,23 @@
+import { APIGet } from './RequestClient'
+
+export enum UsersEndpoint {
+    userRankings = "/users/prediction_rankings"
+}
+
+
+export interface UserRanking {
+    name: string
+    total_score: number
+}
+
+export interface LeaderboardResponse {
+    users: UserRanking[]
+}
+
+
+export class APIUserFetcher {
+    fetchLeaderBoard =  async(): Promise<UserRanking[]> => {
+        const response: LeaderboardResponse = await APIGet(UsersEndpoint.userRankings)
+        return response.users
+    }
+}
