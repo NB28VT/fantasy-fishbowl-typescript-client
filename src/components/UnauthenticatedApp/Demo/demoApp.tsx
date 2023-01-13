ebc57d55a74be81a02b1f9c144de02322f7def37
Nathan Burgess <burgess.nathan28@gmail.com>
2023-01-13 12:28:47 -0500

    Add base styling to unauthenticated app to match authenticated app
    Duplicates the basic styling overlay and background from the authenticated app in the demo app; the login form/splash page has its own styling so I am not using implementing the overaly at the top app level.
    
    In the future, may want to extract this into a shared parent component; wait until we see if the demo app has some unique overall styling

--- src/components/UnauthenticatedApp/Demo/demoApp.tsx
+++ src/components/UnauthenticatedApp/Demo/demoApp.tsx
@@ -1,16 +1,36 @@
 import { Route, RouteComponentProps, Switch, withRouter } from "react-router-dom";
 import React from 'react'
 import DemoStartPage from "./demoStartPage";
+import { StyleMap, VerticalStack } from "utils/styles";
+import backgroundImage from 'images/papyrus-dark.png'
 
 interface DemoAppProps extends RouteComponentProps<any> {}
 
 class DemoAppContent extends React.Component<DemoAppProps> {
   render(): JSX.Element {
-      return (
+
+    const styles: StyleMap = {
+      container: {
+          background: `url(${backgroundImage})`,
+          color: '#F5ED13',
+          height: '100vh',
+      },
+      content: {
+          overflow: 'auto',
+          margin: '0px 10px',
+          paddingBottom: 60,
+          paddingTop: 60,
+      },
+  }
+    return (
+      <div style={styles.container}>
+        <VerticalStack style={styles.content}>
           <Switch>
               <Route exact path={'/demo/start'} component={DemoStartPage}/>
           </Switch>
-      )
+        </VerticalStack>
+      </div>
+    )
   }
 }
 
